aa1e5523b854e0871d29cc81b0030fe75948dc10
SimonB87 <buryans@gmail.com>
2020-12-16 20:47:12 +0100

    Awesome Quotes App - first set up

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() => runApp(MaterialApp(
+    home: QuotesList(),
+));
+
+class QuotesList extends StatefulWidget {
+  @override
+  _QuotesListState createState() => _QuotesListState();
+}
+
+class _QuotesListState extends State<QuotesList> {
+
+  List<Quote> quotes = [
+    Quote(author: "Oscar Wilder", text: "Be yourself, everyone else is taken."),
+    Quote(author: "Abraham Lincoln", text: "No man has a good enough memory to be a successful liar"),
+    Quote(author: "W. C. Fields", text: "I cook with wine, sometimes I even add it to the food."),
+  ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awesome quotes!"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+      ),
+    );
+  }
+}
---
+++ lib/quote.dart
@@ -0,0 +1,8 @@
+class Quote {
+
+  String text;
+  String author;
+  //constructor:
+  Quote({this.text, this.author});
+
+}
